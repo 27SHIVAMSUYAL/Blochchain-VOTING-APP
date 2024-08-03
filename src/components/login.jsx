f595f2536886b9352b3667fc73953031a8e4bc68
shivam <27shivam.1o1@gmail.com>
2024-08-03 20:44:39 +0530

    NEW VERSION

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -6,7 +6,7 @@ const Login = (props) => {
     return (
         <div className="login-container">
             <h1 className="welcome-message text-center text-orange-700">Welcome to decentralized voting application</h1>
-            <h2 className='text-2xl text-red-200 text-center font-bold'> YOU NEED META MASK ACCOUNT AND EXTENTION IN YOUR BROWSER  </h2>
+            <p className="welcome-message text-xs text-center text-orange-700 ">You need a META MASK account and extention in your browser 🦊🦊</p> 
             <div className="  text-center ">
                 <button className="login-button text-sm text-center rounded bg-blue-200 p-2 hover:bg-green-100 hover:font-bold" onClick={props.connectWallet}>Login Metamask</button>
               
